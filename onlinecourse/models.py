3e0c4d1d9a22ab2bf763811110883b973ad7b79a
Rashid Mahmood <h.rashid66@gmail.com>
2023-08-10 00:10:37 -0400

    changes made from theia lab environment

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -134,4 +134,4 @@ class Choice(models.Model):
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
-    Other fields and methods you would like to design
+#   Other fields and methods you would like to design
