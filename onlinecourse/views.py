efc19bb1bccc2ffc523e72566f263b5d62febc3f
Rashid Mahmood <h.rashid66@gmail.com>
2023-08-10 05:23:12 -0400

    changes made from theia lab environment

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -126,11 +126,11 @@ def submit(request, course_id):
 #def extract_answers(request):
             submitted_anwsers = []
             for key in request.POST:
-            if key.startswith('choice'):
+                if key.startswith('choice'):
             value = request.POST[key]
             choice_id = int(value)
             submitted_anwsers.append(choice_id)
-    return submitted_anwsers
+    return submitted_anwsers        
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
